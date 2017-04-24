37d52b4920c2f3be25534ff60959faf73593d4cb
penrodsheh <penrodsheh@localhost.localdomain>
2017-04-24 10:43:27 +0800

    init source zentao9.1.1

---
+++ tmp/log/cron.20170424.log.php
@@ -0,0 +1,87 @@
+<?php
+ die();
+9:20:54 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:25:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:30:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:35:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:40:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:45:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:50:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+9:55:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:00:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:05:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:10:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:15:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:20:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:25:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:30:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:35:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+10:40:56 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
---
+++ tmp/log/php.20170424.log.php
@@ -0,0 +1,5 @@
+<?php
+ die();
+?>
+
+09:21:20 Maximum execution time of 30 seconds exceeded in /www/zentao/module/cron/control.php on line 137 when visiting /index.php?m=cron&f=ajaxExec&t=html
